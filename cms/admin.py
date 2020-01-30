796bba2de877a985d6d22948b822bef11829f41d
Sajid16 <sajid.mahboob16@gmail.com>
2020-01-30 14:43:15 +0600

    homepage data passing is done and accessing individual is later

--- cms/admin.py
+++ cms/admin.py
@@ -9,17 +9,20 @@ from cms.models import Contact
 class ContactCustomization(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'info', 'gender', 'image', 'date_added')
     # list_editable = ('name',)
-    list_display_links = ('date_added',) # list_display_links item cannot be in list_editable
-    list_editable = ('gender','name') # here comma has a massive role if tuple has only one item
-    list_per_page = 1 # pagination process
+    list_display_links = ('name',) # list_display_links item cannot be in list_editable
+    list_editable = ('gender',) # here comma has a massive role if tuple has only one item
+    list_per_page = 10 # pagination process
     search_fields = ('name', 'email', 'phone', 'info', 'gender') # added search field to admin panel
     list_filter = ('gender', 'date_added')
 
 
 # Register your models here.
+
 admin.site.register(Contact, ContactCustomization)
 
+
 ############# unregister from admin panel ################
+
 admin.site.unregister(Group)
 # admin.site.unregister(User)
 
--- cms/views.py
+++ cms/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from cms.models import Contact
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'contacts': Contact.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 def detail(request):
