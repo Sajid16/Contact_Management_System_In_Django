ae0af84fa8fa13322045f84eab62dd0823f93d92
Sajid16 <sajid.mahboob16@gmail.com>
2020-02-12 20:17:07 +0600

    crud operation is next now

--- cms/urls.py
+++ cms/urls.py
@@ -3,6 +3,8 @@ from cms import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home_class/', views.HomePageView.as_view(), name='home2'),
     path('detail/<id>/', views.detail, name='detail'),
+    path('detail_class/<pk>/', views.DetailPageView.as_view(), name='detail2'),
     path('search/', views.search, name='search'),
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from cms.models import Contact
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
@@ -8,14 +10,41 @@ def home(request):
     }
     return render(request, 'index.html', context)
 
+#################### creating class based view #########################
+
+class HomePageView(ListView):
+    template_name = 'index.html'
+    model = Contact
+    context_object_name = 'contacts'
+
+class DetailPageView(DetailView):
+    template_name = 'detail.html'
+    model = Contact
+    context_object_name = 'contacts'
+
+####################### class based view end #########################
+
 
 def detail(request, id):
     context = {
-        'contacts': get_object_or_404(Contact, pk=id)
-        # 'contact': COntact.objects.get(pk=id)
+        # 'contacts': get_object_or_404(Contact, pk=id)
+        'contacts': Contact.objects.get(pk=id)
     }
     return render(request, 'detail.html', context)
 
 
 def search(request):
-    return render(request, 'search.html')
+    if request.POST:
+        search_term = request.POST['search_term']
+        # search_result = Contact.objects.filter(name__icontains = search_term) | Contact.objects.filter(phone__icontains = search_term)
+        # search_result = Contact.objects.filter(name__contains = search_term)
+        search_result = Contact.objects.filter(Q(name__icontains = search_term) | 
+                                               Q(phone__iexact = search_term) |
+                                               Q(info__icontains = search_term) |
+                                               Q(email__iexact = search_term))
+        context = {
+            'search_term': search_term,
+            'contacts': search_result
+        }
+
+    return render(request, 'search.html', context)
