b1258da8cd27bba70b859963b5cc94c1e7272f50
Sajid16 <sajid.mahboob16@gmail.com>
2020-02-26 13:15:16 +0600

    mixins and decorators done

--- cms/urls.py
+++ cms/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from cms import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home_class/', views.HomePageView.as_view(), name='home2'),
-    path('detail/<id>/', views.detail, name='detail'),
-    path('detail_class/<pk>/', views.DetailPageView.as_view(), name='detail2'),
+    # path('', views.home, name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
+    #path('detail/<id>/', views.detail, name='detail'),
+    path('detail/<pk>/', views.DetailPageView.as_view(), name='detail'),
     path('search/', views.search, name='search'),
     path('delete/<id>/', views.Delete, name='delete'),
     path('contacts/create/', views.ContactCreateView.as_view(), name='create'),
--- cms/views.py
+++ cms/views.py
@@ -4,77 +4,40 @@ from django.views.generic import ListView, DetailView
 from django.db.models import Q
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin #this is only for class based views
+from django.contrib.auth.decorators import login_required #this is used for function based views
+from django.views.decorators.http import require_POST
 from cms import test_function
 
 # Create your views here.
-def home(request):
+# def home(request):
 
-    ############ getting username who is logged in ###############
+#     ############ getting username who is logged in ###############
     
-    username = None
-    username = request.user
-    print(username)
+#     username = None
+#     username = request.user
+#     print(username)
 
-    ##############################################################
+#     ##############################################################
 
-    ############## testing external py file's function in views #####################
+#     ############## testing external py file's function in views #####################
 
-    r = test_function.addition(5,6)
-    print("the value of r is: ", r)
+#     r = test_function.addition(5,6)
+#     print("the value of r is: ", r)
 
-    ################# testing external py file's class based function on views #############
+#     ################# testing external py file's class based function on views #############
 
-    p1 = test_function.sajid(30, 40)
-    s = p1.add()
-    print("the value of s from the class based function is: ", s)
+#     p1 = test_function.sajid(30, 40)
+#     s = p1.add()
+#     print("the value of s from the class based function is: ", s)
 
-    context = {
-        'contacts': Contact.objects.all()
-    }
-    return render(request, 'index.html', context)
-
-#################### creating class based view #########################
-
-class HomePageView(ListView):
-    template_name = 'index.html'
-    model = Contact
-    context_object_name = 'contacts'
-
-class DetailPageView(DetailView):
-    template_name = 'detail.html'
-    model = Contact
-    context_object_name = 'contacts'
-
-class ContactCreateView(CreateView):
-    model = Contact
-    template_name = 'create.html'
-    fields = ['name','email','phone','info','gender','image']
-    success_url = '/'
-
-class ContactUpdateView(UpdateView):
-    model = Contact
-    template_name = 'update.html'
-    fields = ['name','email','phone','info','gender','image']
-    
-    def form_valid(self, form):
-        instance = form.save()
-        return redirect('detail', instance.pk)
-
-# class ContactDeleteView(DeleteView):
-#     model = Contact
-#     template_name = 'delete.html'
-#     success_url = '/'
-
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    template_name = "registration/signup.html"
-    success_url = '/'
+#     context = {
+#         'contacts': Contact.objects.all()
+#     }
+#     return render(request, 'index.html', context)
 
 
-
-####################### class based view end #########################
-
-
+@login_required
 def detail(request, id):
     context = {
         # 'contacts': get_object_or_404(Contact, pk=id)
@@ -83,6 +46,8 @@ def detail(request, id):
     return render(request, 'detail.html', context)
 
 
+@login_required
+#@require_POST
 def search(request):
     if request.POST:
         search_term = request.POST['search_term']
@@ -100,7 +65,51 @@ def search(request):
     return render(request, 'search.html', context)
 
 
+@login_required
 def Delete(request, id):
     contact_information = Contact.objects.get(pk = id)
     contact_information.delete()
     return redirect('home')
+
+
+
+#################### creating class based view #########################
+
+class HomePageView(LoginRequiredMixin, ListView):
+    template_name = 'index.html'
+    model = Contact
+    context_object_name = 'contacts'
+
+class DetailPageView(LoginRequiredMixin, DetailView):
+    template_name = 'detail.html'
+    model = Contact
+    context_object_name = 'contacts'
+
+class ContactCreateView(LoginRequiredMixin, CreateView):
+    model = Contact
+    template_name = 'create.html'
+    fields = ['name','email','phone','info','gender','image']
+    success_url = '/'
+
+class ContactUpdateView(LoginRequiredMixin, UpdateView):
+    model = Contact
+    template_name = 'update.html'
+    fields = ['name','email','phone','info','gender','image']
+    
+    def form_valid(self, form):
+        instance = form.save()
+        return redirect('detail', instance.pk)
+
+# class ContactDeleteView(LoginRequiredMixin, DeleteView):
+#     model = Contact
+#     template_name = 'delete.html'
+#     success_url = '/'
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+    success_url = '/'
+
+
+
+####################### class based view end #########################
